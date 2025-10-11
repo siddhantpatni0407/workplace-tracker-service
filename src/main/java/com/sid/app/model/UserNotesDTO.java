32aa0cd95d2c98ca3b967dc3841329ff5407af37
Siddhant Patni <siddhant4patni@gmail.com>
2025-10-12 02:41:36 +0530

    Add JsonProperty annotations to UserNotesDTO fields
    Added @JsonProperty annotations to all fields in UserNotesDTO to explicitly define JSON property names for serialization and deserialization. Also renamed 'noteId' to 'userNoteId' for consistency.

--- src/main/java/com/sid/app/model/UserNotesDTO.java
+++ src/main/java/com/sid/app/model/UserNotesDTO.java
@@ -1,6 +1,7 @@
 package com.sid.app.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sid.app.enums.NoteCategory;
 import com.sid.app.enums.NoteColor;
 import com.sid.app.enums.NotePriority;
@@ -19,53 +20,70 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode
 public class UserNotesDTO {
 
-    private Long noteId;
+    @JsonProperty("userNoteId")
+    private Long userNoteId;
 
+    @JsonProperty("userId")
     private Long userId;
 
+    @JsonProperty("noteTitle")
     @NotBlank(message = "Note title is required")
     @Size(max = 500, message = "Note title must not exceed 500 characters")
     private String noteTitle;
 
+    @JsonProperty("noteContent")
     @NotBlank(message = "Note content is required")
     private String noteContent;
 
+    @JsonProperty("noteType")
     @Builder.Default
     private NoteType noteType = NoteType.TEXT;
 
+    @JsonProperty("color")
     @Builder.Default
     private NoteColor color = NoteColor.DEFAULT;
 
+    @JsonProperty("category")
     @Builder.Default
     private NoteCategory category = NoteCategory.PERSONAL;
 
+    @JsonProperty("priority")
     @Builder.Default
     private NotePriority priority = NotePriority.MEDIUM;
 
+    @JsonProperty("status")
     @Builder.Default
     private NoteStatus status = NoteStatus.ACTIVE;
 
+    @JsonProperty("isPinned")
     @Builder.Default
     private Boolean isPinned = false;
 
+    @JsonProperty("isShared")
     @Builder.Default
     private Boolean isShared = false;
 
+    @JsonProperty("reminderDate")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime reminderDate;
 
+    @JsonProperty("version")
     @Builder.Default
     private Integer version = 1;
 
+    @JsonProperty("accessCount")
     @Builder.Default
     private Integer accessCount = 0;
 
+    @JsonProperty("lastAccessedDate")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime lastAccessedDate;
 
+    @JsonProperty("createdDate")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdDate;
 
+    @JsonProperty("modifiedDate")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime modifiedDate;
 }
