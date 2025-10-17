7a5650e5130cc4d3b41c25ab2b8f4d0687af1e3b
Siddhant Patni <siddhant4patni@gmail.com>
2025-10-17 18:03:42 +0530

    Enable Spring Data Web support in main application
    Added @EnableSpringDataWebSupport annotation with VIA_DTO page serialization mode to WorkplaceTrackerServiceApplication. This enables advanced Spring Data web features such as pageable and sort support in web requests.

--- src/main/java/com/sid/app/WorkplaceTrackerServiceApplication.java
+++ src/main/java/com/sid/app/WorkplaceTrackerServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAspectJAutoProxy
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class WorkplaceTrackerServiceApplication {
 
     public static void main(String[] args) {
